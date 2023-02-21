85cd1db02e579deee97d1a4445b3cd900c7d0049
Nkararam <carolinekararam@gmail.com>
2023-02-21 09:44:52 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,6 +7,7 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
+
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
@@ -16,4 +17,4 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 	i++;
 	}
-}	
+}
