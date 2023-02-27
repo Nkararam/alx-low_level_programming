7c7948c4958704ea4e71466362852a8180215105
Nkararam <carolinekararam@gmail.com>
2023-02-27 12:08:33 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,27 +7,26 @@
 int _atoi(char *s)
 {
 
-int c = 0;
+int C = 0;
 unsigned int ni = 0;
 int min = 1;
-int iSi = 0;
+int isi = 0;
 
-while (S[c])
+while (s[c])
 {
-if (S[c] == 45)
+if (s[c] == 45)
 {
 min *= -1;
 }
-while (S[c] >= 48 && S[c] <= 57)
+while (s[c] >= 48 && s[c] <= 57)
 {
-
-ni = (ni * 10) + (S[c] - '0');
-C++;
+ni = (ni * 10) + (s[c] - '0');
+c++;
 }
-if (iSi == 1)
+if (isi == 1)
 {
 break;
 }
-C++;
+c++;
 }
-
+}
