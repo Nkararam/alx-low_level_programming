114c6543ec3e9c9a1d59b156555511282f62a063
Nkararam <carolinekararam@gmail.com>
2023-03-02 12:34:48 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ int index = 0;
 
 while (str[index])
 {
-while (!(str[index] >= 'a' && str[index] <= 'z'
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 
 index++;
 
@@ -29,7 +29,9 @@ str[index - 1] == '{' ||
 str[index - 1] == '}' ||
 index == 0)
 str[index] -= 32;
+
 index++;
+
 }
 return (str);
 }
