e593f6768b8810875bd438b41a4a9c0482a81a84
Nkararam <carolinekararam@gmail.com>
2023-03-11 06:14:02 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - print its name
+ * main - entry point
+ * description: program that prints its name, followed by a new line.
  * @argc: argument count
  * @argv: argument vector
- * Return always 0
+ * Return always 0(success
+ *
  */
+
 int main(int argc, char *argv[])
 {
+
 (void) argc;
+
 printf("%s\n", argv[0]);
+
 return (0);
 }
