f7c71d52d3d88f5ea256dc60ec847222b2c366ec
Nkararam <carolinekararam@gmail.com>
2023-06-20 13:54:25 -0700

    updated task2

--- 0x18-dynamic_libraries/_puts.c
+++ 0x18-dynamic_libraries/_puts.c
@@ -1,11 +1,7 @@
 #include "main.h"
-#include <stdio.h>
+#include<stdio.h>
 
 void _puts(char *str)
-{
-puts(str);
-}
-
 /**
  * main - Entry point
  *
@@ -13,6 +9,6 @@ puts(str);
  */
 int main(void)
 {
-_puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
